4dbff7c1013fa6eff94b3a28bd74e92d2c5ef6c2
KORDONGSEOK <54506544+KORDONGSEOK@users.noreply.github.com>
2019-12-05 11:20:47 +0900

    jfjfgjhg

---
+++ ksmart_mybatis_Git/src/main/java/kr/or/ksmart/ksmart_mybatis_Git/KsmartMybatisGitApplication.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.ksmart_mybatis_Git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmartMybatisGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmartMybatisGitApplication.class, args);
+	}
+
+}
---
+++ ksmart_mybatis_Git/src/test/java/kr/or/ksmart/ksmart_mybatis_Git/KsmartMybatisGitApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.ksmart_mybatis_Git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsmartMybatisGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
